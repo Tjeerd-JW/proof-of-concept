83715211af398605f594185ff651e9ece28f7dd3
Tjeerd <tj.wokke@gmail.com>
2026-06-03 16:49:24 +0200

    dummydata for homepage added #4

--- server.js
+++ server.js
@@ -20,18 +20,117 @@ app.listen(app.get('port'), function () {
     console.log(`Project draait via http://localhost:${app.get('port')}/\n\nGleep glorp hij staat aan`)
 })
 
-// app.get('/', async function (request, response) {
-//     response.render('index.liquid')
-// })
+const baseURL = 'https://gathering.tweakers.net/rss/'
 
+function randomNumber() {
+  return Math.floor(Math.random() * 750000);
+}
+
+const gameCategories = [
+  {
+  id: 12,
+  title: 'Hardware en spielerij algemeen',
+  topics: randomNumber(),
+  reactions: randomNumber()
+},
+{
+  id: 105,
+  title: 'Actie, adventure en platform games',
+  topics: randomNumber(),
+  reactions: randomNumber()
+},
+{
+  id: 75,
+  title: 'racing',
+  topics: randomNumber(),
+  reactions: randomNumber()
+},
+{
+  id: 106,
+  title: 'Role-playing games',
+  topics: randomNumber(),
+  reactions: randomNumber()
+},
+{
+  id: 104,
+  title: 'Shooters',
+  topics: randomNumber(),
+  reactions: randomNumber()
+},
+{
+  id: 107,
+  title: 'Strategy games',
+  topics: randomNumber(),
+  reactions: randomNumber()
+},
+{
+  id: 108,
+  title: 'Sport en simulatie games',
+  topics: randomNumber(),
+  reactions: randomNumber()
+},
+{
+  id: 53,
+  title: 'Spielerij - problemen en vragen',
+  topics: randomNumber(),
+  reactions: randomNumber()
+}
+]
 
 app.get('/', async function (request, response) {
-
-  const tweakersResponse = await fetch('https://gathering.tweakers.net/rss/list_topics/105')
-  const tweakersResponseXML = await tweakersResponse.text()
-
-  const { format, feed } = parseFeed(tweakersResponseXML)
-  // console.log(feed) // Om te debuggen
-
-  response.render('index.liquid', {items: feed.items})
+    response.render('index.liquid', {
+      gameCategories: gameCategories
+    })
 })
+
+app.get('/categorie/:id', async function (request, response) {
+const rssResponse = await fetch(`${baseURL}list_topics/${request.params.id}`)
+  const responseXML = await rssResponse.text()
+  const { format, feed } = parseFeed(responseXML)
+
+  response.render('category.liquid', {items: feed.items})
+  
+})
+
+
+// app.get('/', async function (request, response) {
+
+//   const tweakersResponse = await fetch('https://gathering.tweakers.net/rss/list_topics/105')
+//   const tweakersResponseXML = await tweakersResponse.text()
+
+//   const { format, feed } = parseFeed(tweakersResponseXML)
+//   // console.log(feed) // Om te debuggen
+
+//   response.render('index.liquid', {items: feed.items})
+// })
+
+// app.get('/', async function (request, response) {
+
+//   const tweakersResponse = await fetch('https://gathering.tweakers.net/rss/list_topics/105')
+//   const tweakersResponseXML = await tweakersResponse.text()
+
+//   const { format, feed } = parseFeed(tweakersResponseXML)
+ 
+
+//   const items = []
+//   for (const item of feed.items) {
+//     items.push({
+//       title: item.title,
+//       link: item.link,
+//       replies: Number(item.description.substring(9, item.description.indexOf('\n')))
+//     })
+//   }
+
+//   items.sort(function(a, b) {
+//    if (a.replies < b.replies) {
+//     return 1;
+//    } else if (a.replies > b.replies) {
+//     return -1;
+//    }
+//    return 0;
+//   })
+
+//   console.log(items)
+
+//   response.render('index.liquid', {items: items})
+// })
