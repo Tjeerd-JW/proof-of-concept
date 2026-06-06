9e94f376e12f47760c8c195a15a8553bd3290d7a
Tjeerd <tj.wokke@gmail.com>
2026-06-06 14:43:26 +0200

    interest invokers with anchor positioning #10

--- public/js/chart.js
+++ public/js/chart.js
@@ -2,6 +2,19 @@ const chart = document.querySelector(".chart");
 const extraInfo = document.querySelector(".extra-info")
 let allDots = document.querySelectorAll('.point')
 
+const supported = Object.hasOwn(
+    HTMLButtonElement.prototype,
+    "interestForElement",
+);
+
+if (!supported) {
+    console.log('geen interest invokers :(')
+}
+if (supported) {
+    console.log(' interest invokers YIPPIEEEE :)')
+
+}
+
 // allDots.forEach(dot => {
 //     dot.addEventListener("mouseenter", showHandler)
 //     dot.addEventListener("mouseleave", leaveHandler)
