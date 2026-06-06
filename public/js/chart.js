335c30d1c276e4c6f3f8e5da451db46026ef8160
Tjeerd <tj.wokke@gmail.com>
2026-06-06 14:05:19 +0200

    new variable names in chart partial #10

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,22 +1,18 @@
-// ontvang data uit liquid
-const chartData = window.chartData;
-
 const chart = document.querySelector(".chart");
 const extraInfo = document.querySelector(".extra-info")
-
 let allDots = document.querySelectorAll('.point')
 
-allDots.forEach(dot => {
-    dot.addEventListener("mouseenter", showHandler)
-    dot.addEventListener("mouseleave", leaveHandler)
-});
+// allDots.forEach(dot => {
+//     dot.addEventListener("mouseenter", showHandler)
+//     dot.addEventListener("mouseleave", leaveHandler)
+// });
 
-function showHandler(event) {
-    console.log('hovered', event)
-    extraInfo.classList.add('show')
-}
+// function showHandler(event) {
+//     console.log('hovered', event)
+//     extraInfo.classList.add('show')
+// }
 
-function leaveHandler(event) {
-    extraInfo.classList.remove('show')
+// function leaveHandler(event) {
+//     extraInfo.classList.remove('show')
 
-}
+// }
